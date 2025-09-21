3075ac3413def7ebcced8cb9cf240516d8f328b5
vijayarun00100 <blockloader78@gmail.com>
2025-09-21 11:28:00 +0530

    adding apollo to react

--- src/signin/Authentication.tsx
+++ src/signin/Authentication.tsx
@@ -1,6 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+import { ApolloProvider, useMutation } from "@apollo/client/react";
 
-const Authentication = () => {
+interface LoginResponse {
+  login: {
+    token: string;
+    user: {
+      id: string;
+      name: string;
+      email: string;
+    };
+  };
+}
+
+const client = new ApolloClient({
+  link: new HttpLink({ 
+    uri: "/query",
+  }),
+  cache: new InMemoryCache(),
+});
+
+const LOGIN_MUTATION = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [login, { data, loading, error }] = useMutation<LoginResponse>(LOGIN_MUTATION);
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const result = await login({
+        variables: { email, password }
+      });
+      console.log('Login successful:', result.data);
+    } catch (err) {
+      console.error('Login failed:', err);
+    }
+  };
+
   return (
     <div className='flex w-full h-screen justify-center items-center bg-gray-300'>
       <div className='flex justify-center items-center bg-gray-100 w-[80%] h-[80%] rounded-2xl p-8 shadow-lg flex-col '>
@@ -12,23 +61,63 @@ const Authentication = () => {
             </div>
           </div>
           <div className='flex flex-col gap-5 mt-2 p-2 justify-center items-center flex-1'>
-            <div className='flex flex-row gap-2 mt-2 p-2 justify-center items-center'>
-              <h1>username :</h1>
-              <input type="text" placeholder='enter username ' className='border-2 border-solid border-black rounded-2xl p-2'/>
-            </div>
-            <div className='flex flex-row gap-2 mt-5 justify-center items-center'>
-              <h1>password :</h1>
-              <input type="password" placeholder='password' className='border-2 border-solid border-black rounded-2xl p-2'/>
-            </div>
-            <div className='flex gap-2 mt-5 justify-center items-center'>
-              <div className='w-24'></div>
-              <button className='bg-blue-500 text-white p-3 rounded-2xl'>Login</button>
-            </div>
+            <form onSubmit={handleLogin} className='flex flex-col gap-5 items-center'>
+              <div className='flex flex-row gap-2 mt-2 p-2 justify-center items-center'>
+                <h1>username :</h1>
+                <input 
+                  type="email" 
+                  placeholder='enter email' 
+                  className='border-2 border-solid border-black rounded-2xl p-2'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className='flex flex-row gap-2 mt-5 justify-center items-center'>
+                <h1>password :</h1>
+                <input 
+                  type="password" 
+                  placeholder='password' 
+                  className='border-2 border-solid border-black rounded-2xl p-2'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className='flex gap-2 mt-5 justify-center items-center'>
+                <div className='w-24'></div>
+                <button 
+                  type="submit" 
+                  className='bg-blue-500 text-white p-3 rounded-2xl disabled:bg-gray-400'
+                  disabled={loading}
+                >
+                  {loading ? 'Logging in...' : 'Login'}
+                </button>
+              </div>
+              {error && (
+                <div className='text-red-500 mt-2'>
+                  Error: {error.message}
+                </div>
+              )}
+              {data?.login && (
+                <div className='text-green-500 mt-2'>
+                  Welcome, {data.login.user.name}!
+                </div>
+              )}
+            </form>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
+};
+
+const Authentication = () => {
+  return (
+    <ApolloProvider client={client}>
+      <LoginForm />
+    </ApolloProvider>
+  );
 }
 
 export default Authentication
